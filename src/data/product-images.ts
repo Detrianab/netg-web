adfa605983d006d35a33fa59ed1b300550f318cd
Detriana <detrianab@gmail.com>
2026-09-16 10:20:12 -0400

    Corrigiendo rutas de las imagenes del catalogo

--- src/data/product-images.ts
+++ src/data/product-images.ts
@@ -1,17 +1,10 @@
-import co2 from "@/assets/product-co2.jpg";
-import dol53 from "@/assets/product-4.png";
-import fujikura from "@/assets/product-7.png";
-import gateway from "@/assets/product-6.png";
-import probe4xt from "@/assets/product-3.png";
-import transmitter from "@/assets/product-5.png";
-
 export const PRODUCT_IMAGES: Record<string, string> = {
-  "gateway-pro": gateway.url,
-  "sensor-co2-temperatura": co2.url,
-  "sonda-4xt": probe4xt.url,
-  "transmisor-0-10v": transmitter.url,
-  "sensor-amoniaco-dol-53": dol53.url,
-  "fujikura-96s": fujikura.url,
+  "gateway-pro": "/product-6.png",
+  "sensor-co2-temperatura": "/product-co2.jpg",
+  "sonda-4xt": "/product-3.png",
+  "transmisor-0-10v": "/product-5.png",
+  "sensor-amoniaco-dol-53": "/product-4.png",
+  "fujikura-96s": "/product-7.png",
 };
 
 export function productImage(slug: string) {
